318db4ada283391cbc6c9e9b11d9bfbdd24b1e3e
Thomas Eby <tgeby@icloud.com>
2026-01-31 14:09:44 -0500

    Implemented simple Express API without persistence

---
+++ 02_expressjs/server.js
@@ -0,0 +1,75 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+const router = express.Router();
+
+app.use(express.json());
+
+let cars = [
+    { id: 1, make: 'Toyota', model: 'Corolla', year: 2015, price: 15000 },
+    { id: 2, make: 'Tesla', model: 'Model S', year: 2023, price: 25000 },
+    { id: 3, make: 'Ford', model: 'F-150', year: 2025, price: 30000 },
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello from the CARS API!');
+});
+
+router.get('/', (req, res) => {
+    res.json(cars);
+});
+
+router.get('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const car = cars.find(car => car.id === id);
+
+    if (!car) return res.status(404).send('Car not found');
+    res.json(cars.find(car => car.id === Number(req.params.id)));
+});
+
+router.post('/', (req, res) => {
+    const nextId = cars.length === 0 ? 1 : Math.max(...cars.map(car => car.id)) + 1;
+
+    const car = {
+        "id": nextId,
+        ...req.body
+    };
+
+    if (!car.make || !car.model || !car.year || !car.price) {
+        return res.status(400).send('Please provide make, model, year, and price');
+    }
+
+    cars.push(car);
+    res.status(201).json(car);
+});
+
+router.put('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const newCar = {
+        "id": id,
+        ...req.body
+    };
+    if (!newCar.make || !newCar.model || !newCar.year || !newCar.price) {
+        return res.status(400).send('Please provide make, model, year, and price');
+    }
+
+    const carIndex = cars.findIndex(car => car.id === Number(req.params.id));
+    if (carIndex === -1) {
+        cars.push(newCar);
+        return res.status(201).json(newCar);
+    }
+    cars[carIndex] = newCar;
+    res.status(200).json(newCar);
+});
+
+router.delete('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    cars = cars.filter(car => car.id !== id);
+    res.status(204).send('Car deleted successfully');
+});
+
+app.use('/api/v1/cars', router);
+
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
