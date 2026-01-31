77ed0701fce7325f3ce835e98c27e1025d4de330
Thomas Eby <tgeby@icloud.com>
2026-01-31 17:19:16 -0500

    Added logging middleware

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -7,6 +7,12 @@ const router = express.Router();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+   const timestamp = new Date().toISOString();
+   console.log(`[${timestamp} ${req.method} ${req.url}]`);
+   next();
+});
+
 let cars = [
     { id: 1, make: 'Toyota', model: 'Corolla', year: 2015, price: 15000 },
     { id: 2, make: 'Tesla', model: 'Model S', year: 2023, price: 25000 },
